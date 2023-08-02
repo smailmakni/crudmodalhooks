64e3b3c42fb765cb42ff51a17ac6d4e85210a8a0
smailmakni <ismail.makni91@gmail.com>
2023-08-02 14:58:18 +0100

    Commit new version

--- src/components/articles/ArticleList.jsx
+++ src/components/articles/ArticleList.jsx
@@ -1,8 +1,12 @@
 import React, { useMemo } from 'react';
 import { MaterialReactTable } from 'material-react-table';
 import { Box } from '@mui/material';
+import Button from 'react-bootstrap/Button';
 
-const ArticleList = ({products}) => {
+const ArticleList = ({products,deleteProduct}) => {
+  const delprod=(productId,des)=>{
+    deleteProduct(productId,des)
+    }
   const columns = useMemo(
     () => [
       {
@@ -51,6 +55,35 @@ const ArticleList = ({products}) => {
           
           </Box>),
       },
+      {
+        accessorKey: '_id',
+        header: 'actions',
+        size: 100,
+        Cell: ({ cell, row }) => (
+          <div >
+          <Button
+          onClick={() => {
+          console.log("modification ...")
+          }}
+          variant="warning"
+          size="md"
+          className="text-warning btn-link edit"
+          >
+          <i class="fa-solid fa-pen-to-square"></i>
+          </Button>
+          <Button
+          onClick={(e) => {
+          delprod(cell.row.original._id,cell.row.original.designation ,e);
+          }}
+          variant="danger"
+          size="md"
+          className="text-danger btn-link delete"
+          >
+          <i className="fa fa-trash" />
+          </Button>
+          </div>
+          ),
+          },
     ],
     [products],
   )
--- src/components/articles/ArticlesApp.jsx
+++ src/components/articles/ArticlesApp.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
-import { fetchArticles } from '../../services/ArticleService'
+import { fetchArticles , deleteArticle } from '../../services/ArticleService'
 import ArticleList from './ArticleList'
+import { confirmAlert } from 'react-confirm-alert'
+import 'react-confirm-alert/src/react-confirm-alert.css'
 
 
 const ArticlesApp = () => {
@@ -13,8 +15,27 @@ const ArticlesApp = () => {
     .then(res=>setProducts(res.data))
     .catch(err=>console.log(err))
   }
+  const deleteProduct = (productId,des) => {
+    confirmAlert({
+    title: "Confirm delete...",
+    message: " supprimer l' article: " + des,
+    buttons: [
+    {
+    label: 'Oui',
+    onClick: () => deleteArticle(productId)
+    .then(res=>
+    setProducts(products.filter((product) => product._id !== productId)))
+    //.then(console.log("suppression effectuée avec success"))
+    .catch(error=>console.log(error))
+    },
+    {
+    label: 'Non',
+    }
+    ]
+    });
+    }
   return (
-    <ArticleList products={products}/>
+    <ArticleList products={products} deleteProduct={deleteProduct}/>
   )
 }
 
